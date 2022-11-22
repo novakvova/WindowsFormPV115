08d1423507290146c5269e04f9a61ab243b63e31
novakvova <novakvova@gmail>
2022-11-22 18:28:37 +0200

    Add Select file is good

--- _14_CreateDialogWinForms/Products/CreateProductForm.cs
+++ _14_CreateDialogWinForms/Products/CreateProductForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,11 @@ namespace _14_CreateDialogWinForms.Products
         public CreateProductForm()
         {
             InitializeComponent();
+            lvImages.LargeImageList = new ImageList();
+            lvImages.LargeImageList.ImageSize = new Size(90, 65);
+            lvImages.MultiSelect = false;
+            lvImages.InsertionMark.Color= Color.Green;
+            lvImages.AllowDrop = true;
         }
 
         private void btnAddImage_Click(object sender, EventArgs e)
@@ -24,6 +30,12 @@ namespace _14_CreateDialogWinForms.Products
             if(dlg.ShowDialog() == DialogResult.OK)
             {
                 string key = Guid.NewGuid().ToString();
+                ListViewItem item= new ListViewItem();
+                item.Tag = dlg.FileName;
+                item.Text = Path.GetFileName(dlg.FileName);
+                item.ImageKey = key;
+                lvImages.LargeImageList.Images.Add(key, Image.FromFile(dlg.FileName));
+                lvImages.Items.Add(item);
 
             }
         }
